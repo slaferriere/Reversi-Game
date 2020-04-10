f2085423431b2a5de61b82f5ef01a0fe1b23c297
Scott LaFerriere <slaferriere@Scotts-MacBook-Air.local>
2020-04-10 13:40:33 -0700

    First commit -Scott

---
+++ src/view/Reversi.java
@@ -0,0 +1,13 @@
+package view;
+
+import javafx.application.Application;
+
+public class Reversi {
+	
+public static void main(String[] args) {
+		
+		Application.launch(ReversiView.class, args);
+
+	}
+
+}
