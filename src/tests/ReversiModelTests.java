e78af84acb6acecaad412bf6e68cac20ca3af1b6
Trevor <trevorfreudig@email.arizona.edu>
2020-04-15 15:20:31 -0700

    Created test classes for model, controller, and board

---
+++ src/tests/ReversiModelTests.java
@@ -0,0 +1,51 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class collects all of the test methods for ReversiModel.
+ * 
+ * In eclipse, running it should run it under JUnit. Running the Reversi.java class (since
+ * it is our main class) will actually run the program. If you're not using eclipse,
+ * you'll need to run this under JUnit 5. 
+ * 
+ * @author Trevor Freudig, Scott LaFerriere
+ *
+ */
+public class ReversiModelTests {
+
+	@Test
+	void testUpdateSpace() {
+
+	}
+
+	@Test
+	void testGetWhite() {
+
+	}
+
+	@Test
+	void testGetBlack() { 
+		
+	}
+	
+	@Test
+	void testGetXPos() { 
+
+	}
+	
+	@Test
+	void testGetYPos() { 
+
+	}
+	
+	@Test
+	void testGetBoard() { 
+
+	}
+
+}
